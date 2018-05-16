5a7f4e4cd10dd5efa2c1d2d5dccd8cad36961455
Serhii Kravchenko <srh.kravchenko@gmail.com>
2018-05-17 00:08:37 +0300

    half image

--- app/src/main/java/ua/org/skravchenko/generator/MainActivity.java
+++ app/src/main/java/ua/org/skravchenko/generator/MainActivity.java
@@ -1,18 +1,24 @@
 package ua.org.skravchenko.generator;
 
+import android.net.Uri;
+import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-  private android.widget.Button bPlus;
-  private android.widget.Button bMinus;
-  private android.widget.Button bGenerate;
-  private android.widget.TextView tvTactCount;
-  private android.widget.LinearLayout llUpLayout;
-  private android.widget.LinearLayout llDownLayout;
-
+  private Button bPlus;
+  private Button bMinus;
+  private Button bGenerate;
+  private TextView tvTactCount;
+  private LinearLayout llContentLayout;
+  private int tactCount = 1;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -20,26 +26,57 @@ public class MainActivity extends AppCompatActivity {
         WindowManager.LayoutParams.FLAG_FULLSCREEN);
     setContentView(R.layout.activity_main);
 
-    bPlus = (android.widget.Button) findViewById(R.id.bPlus);
-    bMinus = (android.widget.Button) findViewById(R.id.bMinus);
-    bGenerate = (android.widget.Button) findViewById(R.id.bGenerate);
-    tvTactCount = (android.widget.TextView) findViewById(R.id.tvTactCount);
-    llUpLayout = (android.widget.LinearLayout) findViewById(R.id.llUpLayout);
-    llDownLayout = (android.widget.LinearLayout) findViewById(R.id.llDownLayout);
+    bPlus = (Button) findViewById(R.id.bPlus);
+    bMinus = (Button) findViewById(R.id.bMinus);
+    bGenerate = (Button) findViewById(R.id.bGenerate);
+    tvTactCount = (TextView) findViewById(R.id.tvTactCount);
+    llContentLayout = (LinearLayout) findViewById(R.id.llContentLayout);
 
+    bPlus.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        if (tactCount < 8) {
+          tactCount++;
+          tvTactCount.setText(tactCount+"");
+        }
+      }
+    });
+    bMinus.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        if (tactCount != 1) {
+          tactCount--;
+          tvTactCount.setText(tactCount+"");
+        }
+      }
+    });
 
-
-
-}
-
-
-
-
-
-
+    bGenerate.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        if (tactCount <= 4) {
+          llContentLayout.removeAllViews();
+          LinearLayout llRoot = new LinearLayout(getApplicationContext());
+          llRoot.setOrientation(LinearLayout.HORIZONTAL);
+          llRoot.setWeightSum(4);
+          llRoot.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,2));
+          for (int i =0;i<tactCount;i++){
+            ImageButton iElement = new ImageButton(getApplicationContext());
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                1);
+            iElement.setImageResource(R.drawable.ic_launcher_foreground);
+            iElement.setLayoutParams(params);
+            llRoot.addView(iElement);
+          }
+          llContentLayout.addView(llRoot);
+        } else {
+        }
+      }
+    });
 
 
   }
+
+}
 
 
 
